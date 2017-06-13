8435c4d72e533b362f4324764e3bd5b12063196e
seedubgh1 <carltonsaddress@hotmail.com>
2017-06-13 08:00:55 -0700

    initial commit

---
+++ src/questions.js
@@ -0,0 +1,238 @@
+'use strict';
+
+module.exports = {
+  "QUESTIONS_EN_US": [
+    {
+      "What breed of dog was Marley in the film Marley &amp; Me?": [
+        "Labrador Retriever",
+        "Golden Retriever",
+        "Dalmatian",
+        "Shiba Inu"
+      ]
+    },
+    {
+      "Which movie released in 2016 features Superman and Batman fighting?": [
+        "Batman v Superman: Dawn of Justice",
+        "Batman v Superman: Superapocalypse",
+        "Batman v Superman: Black of Knight",
+        "Batman v Superman: Knightfall"
+      ]
+    },
+    {
+      "Who starred as Bruce Wayne and Batman in Tim Burtons 1989 movie Batman?": [
+        "Michael Keaton",
+        "George Clooney",
+        "Val Kilmer",
+        "Adam West"
+      ]
+    },
+    {
+      "What name did Tom Hanks give to his volleyball companion in the film `Cast Away`?": [
+        "Wilson",
+        "Friday",
+        "Jones",
+        "Billy"
+      ]
+    },
+    {
+      "Who directed E.T. the Extra-Terrestrial (1982)?": [
+        "Steven Spielberg",
+        "Stanley Kubrick",
+        "James Cameron",
+        "Tim Burton"
+      ]
+    },
+    {
+      "What is the highest grossing film of all time (without adjusting for inflation)?": [
+        "Avatar",
+        "Jurassic World",
+        "Star Wars: The Force Awakens",
+        "Titanic"
+      ]
+    },
+    {
+      "Which animated movie was first to feature a celebrity as a voice actor?": [
+        "Aladdin",
+        "Toy Story",
+        "James and the Giant Peach",
+        "The Hunchback of Notre Dame"
+      ]
+    },
+    {
+      "When was the movie Con Air released?": [
+        "1997",
+        "1985",
+        "1999",
+        "1990"
+      ]
+    },
+    {
+      "Who wrote and directed the 1986 film Platoon?": [
+        "Oliver Stone",
+        "Francis Ford Coppola",
+        "Stanley Kubrick",
+        "Michael Cimino"
+      ]
+    },
+    {
+      "Who played the female lead in the 1933 film King Kong?": [
+        "Fay Wray",
+        "Jean Harlow",
+        "Vivien Leigh",
+        "Mae West"
+      ]
+    },
+    {
+      "Which actress danced the twist with John Travolta in Pulp Fiction?": [
+        "Uma Thurman",
+        "Kathy Griffin",
+        "Pam Grier",
+        "Bridget Fonda"
+      ]
+    },
+    {
+      "Who starred in the film 1973 movie Enter The Dragon?": [
+        "Bruce Lee",
+        "Jackie Chan",
+        "Jet Li",
+        " Yun-Fat Chow"
+      ]
+    },
+    {
+      "Which movie contains the quote, Say hello to my little friend!?": [
+        "Scarface",
+        "Reservoir Dogs",
+        "Heat",
+        "Goodfellas"
+      ]
+    },
+    {
+      "Who directed the movies Pulp Fiction, Reservoir Dogs and Django Unchained?": [
+        "Quentin Tarantino",
+        "Martin Scorcese",
+        "Steven Spielberg",
+        "James Cameron"
+      ]
+    },
+    {
+      "The Queen song `A Kind Of Magic` is featured in which 1986 film?": [
+        "Highlander",
+        "Flash Gordon",
+        "Labyrinth",
+        "Howard the Duck"
+      ]
+    },
+    {
+      "This movie contains the quote, Nobody puts Baby in a corner,": [
+        "Dirty Dancing",
+        "Three Men and a Baby",
+        "Ferris Bueller&#039;s Day Off",
+        "Pretty in Pink"
+      ]
+    },
+    {
+      "What was the first James Bond film?": [
+        "Dr. No",
+        "Goldfinger",
+        "From Russia With Love",
+        "Thunderball"
+      ]
+    },
+    {
+      "Which of these actors/actresses is NOT a part of the cast for the 2016 movie Suicide Squad?": [
+        "Scarlett Johansson",
+        "Jared Leto",
+        "Will Smith",
+        "Margot Robbie"
+      ]
+    },
+    {
+      "Which movie includes a giant bunny-like spirit who has magic powers including growing trees?": [
+        "My Neighbor Totoro",
+        "Hop",
+        "Rise of the Guardians",
+        "Alice in Wonderland"
+      ]
+    },
+    {
+      "Who in Pulp Fiction says No, they got the metric system there, they wouldnt know what the fuck a Quarter Pounder is,": [
+        "Vincent Vega",
+        "Jules Winnfield",
+        "Jimmie Dimmick",
+        "Butch Coolidge"
+      ]
+    },
+    {
+      "In the movie Spaceballs, what are the Spaceballs attempting to steal from Planet Druidia?": [
+        "Air",
+        "The Schwartz",
+        "Princess Lonestar",
+        "Meatballs"
+      ]
+    },
+    {
+      "What was the first monster to appear alongside Godzilla?": [
+        "Anguirus",
+        "King Kong",
+        "Mothra",
+        "King Ghidora"
+      ]
+    },
+    {
+      "In the movie Gremlins, after what time of day should you not feed Mogwai?": [
+        "Midnight",
+        "Evening",
+        "Morning",
+        "Afternoon"
+      ]
+    },
+    {
+      "When does Rogue One, A Star Wars Story take place chronologically in the series?": [
+        "Between Episode 3 and 4",
+        "After Episode 6",
+        "Before Episode 1",
+        "Between Episode 4 and 5"
+      ]
+    },
+    {
+      "The first rule is, you dont talk about it is a reference to which movie?": [
+        "Fight Club",
+        "The Island",
+        "Unthinkable",
+        "American Pie"
+      ]
+    },
+    {
+      "In the 2012 film, The Lorax, who is the antagonist?": [
+        "Aloysius O&#039;Hare",
+        "Ted Wiggins",
+        "The Once-Ler",
+        "Grammy Norma"
+      ]
+    },
+    {
+      "In the 1992 film Army of Darkness, what name does Ash give to his double-barreled shotgun?": [
+        "Boomstick",
+        "Bloomstick",
+        "Blastbranch",
+        "2-Pump Chump"
+      ]
+    },
+    {
+      "In the 1995 film Balto, who are Steeles accomplices?": [
+        "Kaltag, Nikki, and Star",
+        "Dusty, Kirby, and Ralph",
+        "Nuk, Yak, and Sumac",
+        "Jenna, Sylvie, and Dixie"
+      ]
+    },
+    {
+      "What is the name of the island that Jurassic Park is built on?": [
+        "Isla Nublar",
+        "Isla Sorna",
+        "Isla Muerta",
+        "Isla Pena"
+      ]
+    }
+  ]
+}
