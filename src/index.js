b349845f25c573348d6aab7ef1b9c68124fc75ff
seedubgh1 <carltonsaddress@hotmail.com>
2017-06-13 08:13:11 -0700

    duplicate APP_ID variables

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ var languageString = {
 };
 
 var Alexa = require("alexa-sdk");
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+//var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
